99d5240e205e16caabd620c9946c5f40264992fa
Zawwar Khatri <159942012+Zawwar6@users.noreply.github.com>
2025-04-07 22:22:59 +0500

    Zawwar changfe

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,54 +3,30 @@ import Footer from '../components/Footer';
 
 const ProductGrid = () => {
   const products = [
-    {
-      image: '/prod1.jpg',
-      name: 'PRODUCT NAME',
-      price: '$25.00',
-    },
-    {
-      image: '/prod2.jpg',
-      name: 'PRODUCT NAME',
-      price: '$25.00',
-    },
-    {
-      image: '/prod2.jpg',
-      name: 'PRODUCT NAME',
-      price: '$25.00',
-    },
-    {
-      image: '/prod1.jpg',
-      name: 'PRODUCT NAME',
-      price: '$25.00',
-    },
-    {
-      image: '/prod2.jpg',
-      name: 'PRODUCT NAME',
-      price: '$25.00',
-    },
-    {
-      image: '/prod1.jpg',
-      name: 'PRODUCT NAME',
-      price: '$25.00',
-    },
+    { image: '/prod1.jpg', name: 'PRODUCT NAME', price: '$25.00' },
+    { image: '/prod2.jpg', name: 'PRODUCT NAME', price: '$25.00' },
+    { image: '/prod2.jpg', name: 'PRODUCT NAME', price: '$25.00' },
+    { image: '/prod1.jpg', name: 'PRODUCT NAME', price: '$25.00' },
+    { image: '/prod2.jpg', name: 'PRODUCT NAME', price: '$25.00' },
+    { image: '/prod1.jpg', name: 'PRODUCT NAME', price: '$25.00' },
   ];
 
   return (
     <>
-      <div className="min-h-screen  flex justify-center items-center py-10 px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 w-full max-w-7xl">
+      <div className="min-h-screen flex justify-center items-center py-10 px-2 sm:px-4">
+        <div className="grid grid-cols-2 gap-3 sm:gap-6 w-full max-w-7xl">
           {products.map((product, index) => (
             <div
               key={index}
-              className=" p-4  transition-shadow duration-300"
+              className=" p-2 sm:p-4  transition-transform duration-300 hover:scale-105"
             >
               <img
                 src={product.image}
                 alt={product.name}
-                className="w-full h-[500px] object-cover mb-4 rounded-lg transition-transform duration-300 hover:scale-105"
+                className="w-full h-48 sm:h-[500px] object-cover mb-2 sm:mb-4 rounded-md"
               />
-              <div className="text-black font-bold text-lg">{product.name}</div>
-              <div className="text-gray-700 text-sm mt-1">{product.price}</div>
+              <div className="text-black font-bold text-xs sm:text-lg">{product.name}</div>
+              <div className="text-gray-700 text-xs sm:text-sm mt-1">{product.price}</div>
             </div>
           ))}
         </div>
